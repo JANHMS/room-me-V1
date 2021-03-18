657c8d8982bc976c04352034ef0d35aecb574ee4
JANHMS <jan@kooperativ.net>
2021-03-18 08:00:55 +0100

    refactor: deconstruct LoginPage

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,25 +1,25 @@
-import { IonContent, IonLoading, IonInput, IonIcon, IonButton, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonLoading, IonInput, IonIcon, IonButton, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonText } from '@ionic/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { SignIn } from './GoogleSignInButton';
 import HeaderImage from './HeaderImage';
 
 interface Props {
-  username: string;
-  setUsername: (x: string) => void;
+  email: string;
+  setEmail: (x: string) => void;
   password: string;
   setPassword: (x: string) => void;
-  loading: boolean;
+  status: any;
   handleLogin: any;
   auth: any;
 }
 
 const LoginForm: React.FC<Props> = ({
-  username,
-  setUsername,
+  email,
+  setEmail,
   password,
   setPassword,
-  loading,
+  status,
   handleLogin,
   auth
 }) => {
@@ -27,28 +27,32 @@ const LoginForm: React.FC<Props> = ({
     <IonPage>
       <IonHeader>
         <IonToolbar>
-        <HeaderImage />
+          <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <h2>Login</h2>
-      <IonLoading message="Logging in..." duration={1000} isOpen={loading}/>
-      <IonContent className="icon-padding">
-        <IonInput 
-          value={username} 
-          onIonChange={(e:any) => setUsername(e.target.value)} 
-          placeholder="Username" 
-        />
-        <IonInput 
-          type="password"
-          value={password} 
-          onIonChange={(e:any) => setPassword(e.target.value)} 
-          placeholder="Password" 
-        />
-        <IonButton onClick={handleLogin} expand="full">
-          Login        
+      <IonContent className="ion-padding">
+        <IonList>
+          <IonItem>
+            <IonLabel position="stacked">Email</IonLabel>
+            <IonInput type="email" value={email}
+              onIonChange={(event) => setEmail(event.detail.value)}
+            />
+          </IonItem>
+          <IonItem>
+            <IonLabel position="stacked">Password</IonLabel>
+            <IonInput type="password" value={password}
+              onIonChange={(event) => setPassword(event.detail.value)}
+            />
+          </IonItem>
+        </IonList>
+        {status.error &&
+          <IonText color="danger">Invalid credentials</IonText>
+        }
+        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
+        <IonButton expand="block" fill="clear" routerLink="/register">
+          Don't have an account?
         </IonButton>
-        <SignIn auth={auth}/>
-        <p><Link to="/register">New here?</Link></p>
+        <IonLoading isOpen={status.loading} />
       </IonContent>
     </IonPage>
   )
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -15,6 +15,7 @@ import {
 import React, { useState } from 'react';
 import { Redirect } from 'react-router';
 import { useAuth } from '../auth';
+import LoginForm from '../components/LoginForm';
 import { auth } from '../firebase';
 
 const LoginPage: React.FC = () => {
@@ -38,37 +39,15 @@ const LoginPage: React.FC = () => {
     return <Redirect to="/my/entries" />;
   }
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent className="ion-padding">
-        <IonList>
-          <IonItem>
-            <IonLabel position="stacked">Email</IonLabel>
-            <IonInput type="email" value={email}
-              onIonChange={(event) => setEmail(event.detail.value)}
-            />
-          </IonItem>
-          <IonItem>
-            <IonLabel position="stacked">Password</IonLabel>
-            <IonInput type="password" value={password}
-              onIonChange={(event) => setPassword(event.detail.value)}
-            />
-          </IonItem>
-        </IonList>
-        {status.error &&
-          <IonText color="danger">Invalid credentials</IonText>
-        }
-        <IonButton expand="block" onClick={handleLogin}>Login</IonButton>
-        <IonButton expand="block" fill="clear" routerLink="/register">
-          Don't have an account?
-        </IonButton>
-        <IonLoading isOpen={status.loading} />
-      </IonContent>
-    </IonPage>
+    <LoginForm
+      email={email}
+      setEmail={setEmail}
+      password={password}
+      setPassword={setPassword}
+      status={status}
+      handleLogin={handleLogin}
+      auth={auth}
+    />
   );
 };
 

a28efb2d570a3c980cb172724ebe63d4051ae1ab
JANHMS <jan@kooperativ.net>
2021-03-19 10:25:32 +0100

    refactor: rename to ts

---
+++ src/components/Spinner/SpinnerComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Spinner.css'
+
+const Spinner = () => 
+  <div className="spinner-container">
+    <div className="lds-ripple">
+      <div></div>
+      <div></div>
+    </div>
+  </div>
+
+export default Spinner

65ca41a7a234e49ab723a0139346c75d26c2a570
JANHMS <jan@kooperativ.net>
2021-03-28 17:28:10 +0200

    feat: Joined People

---
+++ src/components/collaboration/JoinedPeople.tsx
@@ -0,0 +1,41 @@
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonContent, IonImg, IonItem, IonList, IonPage } from '@ionic/react'
+import React from 'react'
+
+const JoinedPeople = ({
+  users
+}) => {
+
+
+  const statusClass = (status) => {
+    return status === 'online' ? 'is-success' : 'is-danger'
+  }
+
+  return (
+    (users.length > 0) ?
+    <IonPage>
+      <IonContent>
+        <IonList>
+          {users.map(user => 
+          <IonItem key={user.id}>
+            <IonCard>
+              <IonImg
+                  src={user.avatar}
+                  alt="icon avatar"
+              />
+              <IonCardHeader>
+                <IonCardSubtitle>{user.fullName}</IonCardSubtitle>
+              </IonCardHeader>
+              <IonCardContent>
+                <span className={`tag textItem ${statusClass(user.state)}`}>{user.state}
+                </span>
+              </IonCardContent>
+            </IonCard>
+          </IonItem>
+        )}
+      </IonList>
+    </IonContent>
+  </IonPage> : null
+  )
+}
+
+export default JoinedPeople
